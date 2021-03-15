8c136405c2cefc977266da5d7e44064157f87d6c
qbasob <qbasob@gmail.com>
2021-03-15 21:38:07 +0100

    Change blog information

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Alert from '../components/alert'
+// import Alert from '../components/alert'
 import Footer from '../components/footer'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
-        <Alert preview={preview} />
+        {/* <Alert preview={preview} /> */}
         <main>{children}</main>
       </div>
       <Footer />
